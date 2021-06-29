2fa859c1ca1d8c683dd16247a3a054eb4abe8336
laurenrhuang <laurenrhuang@umail.ucsb.edu>
2021-06-28 21:18:07 -0700

    lauren's practice updates

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 ```ps
 python3 -m venv venv
 .\venv\Scripts\activate
+pip install swapi
+pip freeze > requirements.txt
+# now all we need is:
 pip3 install -r .\requirements.txt
 ```
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 
 if __name__ == '__main__':
     print('Hello world!')
-    
+
     response = requests.get('https://swapi.dev/api/people/1')
     print(response.json())
-
